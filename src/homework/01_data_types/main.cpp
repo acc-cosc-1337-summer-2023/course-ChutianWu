6d2e3c2ed5747cf2c09cde16ca9b45be52b6c03c
ChutianWu <135079977+ChutianWu@users.noreply.github.com>
2023-06-10 02:38:21 +0000

    hw1: multiply_numbers

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,23 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+	int num;
+	cout<<"Please enter a number for num: ";
+	cin>>num;
+
+	int result = multiply_numbers(num);
+	cout<<"The result is: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"The result is: "<<result<<"\n";
 
 	return 0;
 }
